b682238fb56e3b94ade30a039290ab53ae390f2e
Szymon Nowak-Trzos <szymtos@gmail.com>
2023-04-07 01:01:47 +0200

    Another optimalizations

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import time
 
 pygame.init()
 
+# Setting map constants
 scale = 0.75
 
 MAP_WIDTH = 900
 MAP_HEIGHT = 900
+RAY_COUNT = 90  # Actual number of rays is 2*RAY_COUNT-1
+FOV = 90
 
 RENDER_WIDTH = 2 * MAP_WIDTH
 RENDER_HEIGHT = MAP_HEIGHT
@@ -25,30 +28,49 @@ WINDOW_HEIGHT *= scale
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("WOOFenstien")
+font = pygame.font.Font('digital-7/digital-7.ttf', 20)
+
 clock = pygame.time.Clock()
+
 map_scale = 0.4
 game_map = GameMap(screen, MAP_WIDTH, MAP_HEIGHT, [RENDER_WIDTH - MAP_WIDTH * map_scale, 0], map_scale)
+# Adding objects to the map
 game_map.add_object(Box(screen, game_map, 100, 10, 100, 100))
+game_map.add_object(Box(screen, game_map, 10, 10, 100, 100))
 game_map.add_object(Box(screen, game_map, 10, 50, 300, 100))
 game_map.add_object(Box(screen, game_map, MAP_WIDTH, 10, 0, 0))
 game_map.add_object(Box(screen, game_map, MAP_WIDTH, 10, 0, MAP_HEIGHT - 10))
 game_map.add_object(RedBox(screen, game_map, 10, MAP_HEIGHT - 20, 0, 10))
 game_map.add_object(Box(screen, game_map, 10, MAP_HEIGHT - 20, MAP_WIDTH - 10, 10))
-game_map.add_object(Box(screen, game_map, 100, 10, 100, 100))
+game_map.add_object(RedBox(screen, game_map, 10, MAP_HEIGHT - 20, 300, 10))
+game_map.add_object(Box(screen, game_map, 10, MAP_HEIGHT - 20, 300 + 30, 10))
 game_map.add_object(Box(screen, game_map, 10, 10, 300, 300))
-game_map.add_object(Glass(screen, game_map, 10, 100, 200, 100))
-game_map.add_object(Glass(screen, game_map, 100, 100, 50, 350))
+game_map.add_object(GreenGlass(screen, game_map, 10, 100, 200, 100))
+game_map.add_object(GreenGlass(screen, game_map, 100, 100, 50, 350))
 game_map.add_object(Glass(screen, game_map, 100, 10, 350, 350))
 game_map.add_object(Box(screen, game_map, 10, 50, 400, 390))
-game_map.add_object(Glass(screen, game_map, 10, 100, 150, 120))
-game_map.add_player(Player(screen, game_map, 10, 10, MAP_WIDTH // 2 - 20, MAP_HEIGHT // 2+200))
+game_map.add_object(RedGlass(screen, game_map, 10, 100, 150, 120))
+game_map.add_object(Glass(screen, game_map, 5, 100, 550, 100))
+game_map.add_object(GreenGlass(screen, game_map, 5, 100, 540, 100))
+game_map.add_object(RedGlass(screen, game_map, 5, 100, 530, 100))
+# game_map.add_object(Box(screen, game_map, 5, 5, MAP_WIDTH // 2 - 20, MAP_HEIGHT // 2 + 100))
 
-renderer = Renderer(game_map, RENDER_WIDTH, 103, 100)
+# Adding player to the map
+game_map.add_player(Player(screen, game_map, 5, 5, MAP_WIDTH // 2 - 20, MAP_HEIGHT // 2 + 200))
+# game_map.add_player(Player(screen, game_map, 5, 5, 555, 150, 90))
+
+renderer = Renderer(game_map, RENDER_WIDTH, FOV, RAY_COUNT)
 
 pygame.event.set_grab(True)
 pygame.mouse.set_visible(False)
+
+start = time.time()
+i = 0
 while True:
-    pygame.mouse.set_pos = (0, 0)
+    # if time.time() - start > 10:
+    #     pygame.quit()
+    #     exit()
+    # pygame.mouse.set_pos = (0, 0)
     mouse_move = (0, 0)
 
     for event in pygame.event.get():
@@ -61,7 +83,11 @@ while True:
                 exit()
         if event.type == pygame.MOUSEMOTION:
             mouse_move = event.rel
+        if event.type == pygame.MOUSEWHEEL:
+            i += event.y
+
     game_map.player.sprite.rotate_mouse(mouse_move)
+
     renderer.generate_rays(game_map.player.sprite.x + game_map.player.sprite.width / 2,
                            game_map.player.sprite.y + game_map.player.sprite.height / 2,
                            game_map.player.sprite.orientation)
@@ -69,6 +95,14 @@ while True:
     game_map.draw()
     game_map.update()
     renderer.draw_rays()
+    # renderer.draw_visible_edges(game_map.player.sprite.x + game_map.player.sprite.width / 2,
+    #                             game_map.player.sprite.y + game_map.player.sprite.height / 2,
+    #                             game_map.player.sprite.orientation)
+
+    # Show FPS
+    text = font.render(str(int(clock.get_fps())), True, "white")
+    screen.blit(text, (10, 10))
+
     pygame.display.update()
-    print(clock.get_fps())
+
     clock.tick(60)
--- renderer.py
+++ renderer.py
@@ -3,42 +3,51 @@ import math
 import pygame
 
 import map
+import random
 
 
 class Renderer:
     def __init__(self, game_map, width3d, fov, ray_count):
-        # self.colors = {obj_examples[i].type: obj_examples[i].color for i in range(len(obj_examples))}
         self.width3d = width3d
         self.game_map = game_map
         self.fov = fov
-        self.ray_count = ray_count
+        self.ray_count = ray_count  # Actual number of rays is 2*ray_count-1
         self.ray_collisions = []
         self.obj_col = []
+
         self.angles = [math.atan(i / ray_count * math.tan(fov / 2 * math.pi / 180)) * 180 / math.pi for i in
-                       range(-ray_count, ray_count + 1)]
-        # print(self.angles)
-        self.scales = [math.cos(a * math.pi / 180) for a in self.angles]
+                       range(-ray_count, ray_count + 1)]  # Angle between player orientation and each ray
+        self.scales = [1 / math.cos(a * math.pi / 180) for a in self.angles]  # Scaling factor for each ray
+
         self.bg = pygame.Surface((self.width3d, self.game_map.height))
         self.bg.fill((0, 0, 0))
-        self.cell = pygame.Surface((self.width3d, self.game_map.height / 2))
+
+        self.ceil = pygame.Surface((self.width3d, self.game_map.height / 2))
+        self.ceil_color = hex_to_rgb("#97b4b7")
 
         self.floor = pygame.Surface((self.width3d, self.game_map.height / 2))
-        self.cell_color = hex_to_rgb("#7E7463")
-        self.floor_color = hex_to_rgb("#525252")
-        self.cell.fill("#7E7463")
-        self.floor.fill("#525252")
+        self.floor_color = hex_to_rgb("#6d6a50")
+
+        self.ceil.fill(self.ceil_color)
+        self.floor.fill(self.floor_color)
+
+        # Default camera coordinates
         self.x0 = 0
         self.y0 = 0
-        # self.scales = [1 for a in self.angles]
 
     def generate_data(self, x0, y0, orient):
         self.x0 = x0
         self.y0 = y0
+
+        # Generating rays' endpoints
         r = 1000000
         circle = [[r * math.sin((orient + self.angles[abs(i)] + 180) * math.pi / 180) + x0,
                    r * math.cos((orient + self.angles[abs(i)] + 180) * math.pi / 180) + y0]
                   for i in range(len(self.angles))]
+
         objects = self.game_map.objects.sprites()
+
+        # Due to all objects being rectangles we can check only the collisions between rays and edges
         object_corners = []
         for obj in objects:
             object_corners.append(
@@ -47,30 +56,62 @@ class Renderer:
                  [obj.x, obj.y + obj.height], obj,
                  [[obj.width, 0], [0, obj.height], [-obj.width, 0], [0, -obj.height]]])
 
+        # We can disregard not visible corners filtering by the relative position of the player to the object and
+        # by the direction the camera is facing
         to_check = [[] for _ in range(len(objects))]
-        up = not (0 <= orient <= 90 - self.fov / 2 or 270 + self.fov / 2 <= orient <= 360)
-        down = not (90 + self.fov / 2 <= orient <= 270 - self.fov / 2)
-        right = not (180 + self.fov / 2 <= orient <= 360 - self.fov / 2)
-        left = not (0 + self.fov / 2 <= orient <= 180 - self.fov / 2)
+        # Checking camera's direction
+        up = (0 <= orient <= 90 - self.fov / 2 or 270 + self.fov / 2 <= orient <= 360)
+        down = (90 + self.fov / 2 <= orient <= 270 - self.fov / 2)
+        right = (180 + self.fov / 2 <= orient <= 360 - self.fov / 2)
+        left = (0 + self.fov / 2 <= orient <= 180 - self.fov / 2)
+
         i = 0
         for obj in self.game_map.objects.sprites():
             obj_center = (obj.x + obj.width / 2, obj.y + obj.height / 2)
-            if not ((not up) and obj.y > y0 or (not down) and obj.y + obj.height < y0 or
-                    (not right) and obj.x + obj.width < x0 or (not left) and obj.x > x0):
-                if obj_center[0] - x0 > 0 and left:
+            # print(not (up and obj.y > y0 or down and obj.y + obj.height < y0 or
+            #         right and obj.x + obj.width < x0 or left and obj.x > x0))
+            if not (up and obj.y > y0 or down and obj.y + obj.height < y0 or
+                    right and obj.x + obj.width < x0 or left and obj.x > x0):
+                if obj_center[0] - x0 > 0 and not left:
                     to_check[i].append([3, 0])
-                elif right:
+                elif not right:
                     to_check[i].append([1, 2])
-                if obj_center[1] - y0 < 0 and down:
+                if obj_center[1] - y0 < 0 and not down:
                     to_check[i].append([2, 3])
-                elif up:
+                elif not up:
                     to_check[i].append([0, 1])
+            n = len(to_check[i])
+            circle_size = 2
+            if n == 1:
+                # pygame.draw.circle(self.game_map.screen, "yellow", [object_corners[i][to_check[i][0][0]][0] * self.game_map.scale + self.game_map.vector[0],
+                #                                                   object_corners[i][to_check[i][0][0]][1]] * self.game_map.scale + self.game_map.vector[1], circle_size)
+                #
+                # pygame.draw.circle(self.game_map.screen, "yellow", [object_corners[i][to_check[i][0][1]][0] * self.game_map.scale + self.game_map.vector[0],
+                #                                                   object_corners[i][to_check[i][0][1]][1] * self.game_map.scale + self.game_map.vector[1]], circle_size)
+
+                if not (is_visible(object_corners[i][to_check[i][0][0]], [x0, y0], orient, self.fov) or
+                        is_visible(object_corners[i][to_check[i][0][1]], [x0, y0], orient, self.fov)):
+                    pass
+                    # to_check[i] = []
+            elif n > 1:
+                tmp = [k for k in to_check[i][0] if k not in to_check[i][1]] + \
+                      [k for k in to_check[i][1] if k not in to_check[i][0]]
+                # pygame.draw.circle(self.game_map.screen, "yellow", [object_corners[i][tmp[0]][0] * self.game_map.scale + self.game_map.vector[0],
+                #                                                   object_corners[i][tmp[0]][1] * self.game_map.scale + self.game_map.vector[1]], circle_size)
+                # pygame.draw.circle(self.game_map.screen, "yellow", [object_corners[i][tmp[1]][0] * self.game_map.scale + self.game_map.vector[0],
+                #                                                   object_corners[i][tmp[1]][1] * self.game_map.scale + self.game_map.vector[1]], circle_size)
+                # print(tmp)
+                # print((orient + self.fov / 2) % 360, orient, is_visible(object_corners[i][tmp[0]], [x0, y0], orient, self.fov),
+                #       (orient - self.fov / 2) % 360)
+                if not (is_visible(object_corners[i][tmp[0]], [x0, y0], orient, self.fov) or
+                        is_visible(object_corners[i][tmp[1]], [x0, y0], orient, self.fov)):
+                    to_check[i] = []
             i += 1
         return circle, object_corners, to_check
 
     def generate_rays(self, x0, y0, orient):
-        scanline = [[x0, y0], [1000000 * math.sin((orient + 180) * math.pi / 180) + x0,
-                               1000000 * math.cos((orient + 180) * math.pi / 180) + y0]]
+        # scanline = [[x0, y0], [1000000 * math.sin((orient + 180) * math.pi / 180) + x0,
+        #                        1000000 * math.cos((orient + 180) * math.pi / 180) + y0]]
         circle, object_corners, to_check = self.generate_data(x0, y0, orient)
         i = 0
         self.ray_collisions = [[] for _ in circle]
@@ -83,13 +124,10 @@ class Renderer:
                 intersections = []
                 for pair in obj_corner_pairs:
                     edge = [object_corners[j][pair[0]], object_corners[j][pair[1]]]
-                    #     # pygame.draw.line(self.game_map.screen, pygame.Color(222, 0, 0, 255),
-                    #     #                  [edge[0][0] * self.game_map.scale + self.game_map.vector[0],
-                    #     #                   edge[0][1] * self.game_map.scale + self.game_map.vector[1]],
-                    #     #                  [edge[1][0] * self.game_map.scale + self.game_map.vector[0],
-                    #     #                   edge[1][1] * self.game_map.scale + self.game_map.vector[1]], 3)
                     intersection = line_intersection(ray, edge)
-                    intersections.append([intersection, object_corners[j][4], angle_between_lines(ray_vec, pair[0])])
+                    intersections.append([intersection, object_corners[j][4],
+                                          angle_between_lines(ray_vec,
+                                                              pair[0], self.angles[i] + orient)])
                 #
                 if intersections:
                     collisions.append(min(intersections, key=lambda x: distance_no_sqrt([x0, y0], x[0])))
@@ -97,11 +135,13 @@ class Renderer:
                 j += 1
             #
             collisions.sort(key=lambda x: distance_no_sqrt([x0, y0], x[0]))
+
             collisions_proper = []
             j = 0
-            while j < len(collisions) and collisions[j][1].type == "Glass":
+            while j < len(collisions) and collisions[j][1].type == "semitransparent":
                 collisions_proper.append(collisions[j])
                 j += 1
+            # collisions_proper.sort(key=lambda x: (random.random() * 323))
             if j < len(collisions):
                 collisions_proper.append(collisions[j])
             self.ray_collisions[i] = collisions_proper
@@ -124,22 +164,38 @@ class Renderer:
                 # c_glass = hex_to_rgb("#247F93") + [100]
                 # c_glass = (0, 0, 255, 50)
                 # c = (255, 255, 255, 120)
-                c = (255, 0, 0, 120)
+                # c = (255, 0, 0, 120)
+                c = (255, 255, 255, 1)
                 j = 0
                 for line in lines:
                     if j > 0:
                         c = combine_colors(lines[j - 1][2].color, c)
-
+                    size = 1
+                    if i == int(len(self.ray_collisions) / 2):
+                        size = 3
                     pygame.draw.line(self.game_map.screen, pygame.Color(c),
-                                     line[0], line[1])
+                                     line[0], line[1], size)
                     j += 1
         i += 1
 
-    def render(self):
-        self.game_map.screen.blit(self.cell, (0, 0))
+    def draw_visible_edges(self, x0, y0, orient):
+        circle, object_corners, to_check = self.generate_data(x0, y0, orient)
+        j = 0
+        for obj_corner_pairs in to_check:
+            for pair in obj_corner_pairs:
+                edge = [object_corners[j][pair[0]], object_corners[j][pair[1]]]
+                pygame.draw.line(self.game_map.screen, pygame.Color(222, 222, 222, 255),
+                                 [edge[0][0] * self.game_map.scale + self.game_map.vector[0],
+                                  edge[0][1] * self.game_map.scale + self.game_map.vector[1]],
+                                 [edge[1][0] * self.game_map.scale + self.game_map.vector[0],
+                                  edge[1][1] * self.game_map.scale + self.game_map.vector[1]], 1)
+            j += 1
+
+    def render(self, i=0):
+        self.game_map.screen.blit(self.ceil, (0, 0))
         self.game_map.screen.blit(self.floor, (0, self.game_map.height / 2))
         # print(self.rays)
-        const = self.game_map.height * 30
+        const = self.game_map.height * 50
 
         # distances = [[[self.scales[i] * distance([self.x0, self.y0], self.ray_collisions[i][j][0]),
         #                self.ray_collisions[i][j][1], self.ray_collisions[i][j][2]]
@@ -155,6 +211,8 @@ class Renderer:
         # c_box = hex_to_rgb("#A8A8A8") + [122]
         # c_glass = hex_to_rgb("#2A97B0") + [122]
         c = (0, 0, 0, 255)
+        # h = -400
+        h = 0
         for i in range(len(distances_normalised)):
             for j in range(len(distances_normalised[i])):
                 if j == 0:
@@ -163,27 +221,49 @@ class Renderer:
                     c = combine_colors(c, distances_normalised[i][j][1].color)
                 b = 12 / (j + 4) * max(0, min(255, abs(distances_normalised[i][j][2]) / 360 * 255))
                 c = combine_colors([220 - b, 220 - b, 220 - b, 60], c)
-                # pygame.draw.rect(self.game_map.screen, pygame.Color(c),
-                #                  pygame.Rect(self.width3d - (i + 1) * dw, self.game_map.height / 2 -
-                #                              (const / (distances_normalised[i][j][0])) / 2, dw + 1,
-                #                              const / (distances_normalised[i][j][0]) / 2))
-                # pygame.draw.rect(self.game_map.screen, pygame.Color(c),
-                #                  pygame.Rect(self.width3d - (i + 1) * dw, self.game_map.height / 2 - 1, dw + 1,
-                #                              const / distances_normalised[i][j][0] / 2))
-                pygame.draw.rect(self.game_map.screen, pygame.Color(c),
-                                 pygame.Rect(self.width3d - (i + 1) * dw, self.game_map.height / 2 -
-                                             (const / (distances_normalised[i][j][0])) / 2, dw + 1,
-                                             const / (distances_normalised[i][j][0])))
+                b = 12 / (j + 4) * max(0, min(255, abs(1 / distances_normalised[i][j][0]) ** 2 / 7000))
+                c = [max(max(0, i - 70), i - b / 4) for i in c]
+                if distances_normalised[i][j][1].type == "semitransparent":
+                    sat = 1.5
+                    cu = combine_colors(self.ceil_color + [60], c)
+                    cd = combine_colors(self.floor_color + [60], c)
+                    cu = saturation(cu, sat)
+                    cd = saturation(cd, sat)
+                    pygame.draw.rect(self.game_map.screen, pygame.Color(cu),
+                                     pygame.Rect(self.width3d - (i + 1) * dw, self.game_map.height / 2 -
+                                                 (const * (distances_normalised[i][j][0])) / 2 + h, dw + 1,
+                                                 const * (distances_normalised[i][j][0]) / 2))
+                    pygame.draw.rect(self.game_map.screen, pygame.Color(cd),
+                                     pygame.Rect(self.width3d - (i + 1) * dw, self.game_map.height / 2 - 1 + h, dw + 1,
+                                                 const * distances_normalised[i][j][0] / 2))
+                else:
+                    pygame.draw.rect(self.game_map.screen, pygame.Color(c),
+                                     pygame.Rect(self.width3d - (i + 1) * dw, self.game_map.height / 2 -
+                                                 (const * (distances_normalised[i][j][0])) / 2 + h, dw + 1,
+                                                 const * (distances_normalised[i][j][0])))
 
 
 def distance(p1, p2):
     # return distance_2(p1, p2)
-    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+    return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** -0.5
+
+
+def is_visible(obj_center, player, orient, fov):
+    vec = [obj_center[0] - player[0], obj_center[1] - player[1]]
+    a = ((DiamondAngle(vec[0], vec[1]) - 2) * 90) % 360
+    # print("a:", a)
+    # print((a - orient + fov/2)%360)
+    acc = 5
+    return 10 - acc <= (a - orient + fov / 2 + 10) % 360 <= 10 + 180 + acc
+    # if (orient + fov / 2 + acc) % 360 > (orient - fov / 2 - acc) % 360:
+    #     return not ((orient + fov / 2 + acc) % 360 <= a <= 0 or 0 <= a <= (orient - fov / 2 - acc) % 360)
+    # else:
+    #     return ((orient + fov / 2 + acc) % 360 <= a <= (orient - fov / 2 - acc) % 360)
 
 
 def distance_no_sqrt(p1, p2):
     # return distance_2(p1, p2)
-    return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
+    return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
     # return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
@@ -200,7 +280,7 @@ def distance_2(p1, p2):
     return (dx + dy - (dy >> 1)) * m
 
 
-def angle_between_lines(v1, edge_index):
+def angle_between_lines(v1, edge_index, angle):
     # v1 = [l1[1][0] - l1[0][0], l1[1][1] - l1[0][1]]
     # v2 = [l2[1][0] - l2[0][0], l2[1][1] - l2[0][1]]
     # a = (math.acos(((v1[0] * v2[0] + v1[1] * v2[1]) /
@@ -220,7 +300,13 @@ def angle_between_lines(v1, edge_index):
     #     a = ((DiamondAngle(v1[1], v1[0]) + 1 + 2) / 4 * 360) % 180 - 90
     # else:
     #     a = 180
-    a = ((DiamondAngle(v1[1], v1[0]) + abs(edge_index-1)-1) * 90) % 180 - 90
+    # a = ((DiamondAngle(v1[1], v1[0]) + abs(edge_index-1)-1) * 90) % 180 - 90
+    # print("DIA", (DiamondAngle(v1[1], v1[0])))
+    # print("and", (-angle/90 + 3))
+
+    # a = (((-angle / 90 + 3) + (abs(edge_index - 1) - 1)) * 90) % 180 - 90
+    a = (90 * abs(edge_index - 1) - angle) % 180 - 90
+
     # print(a)
     return a
 
@@ -309,15 +395,25 @@ def combine_colors(c0, c1):
     r01 = ((1 - a0) * a1 * r1 + a0 * r0) / a01
     g01 = ((1 - a0) * a1 * g1 + a0 * g0) / a01
     b01 = ((1 - a0) * a1 * b1 + a0 * b0) / a01
-    r01 = max(0, r01 - 15)
-    g01 = max(0, g01 - 15)
-    b01 = max(0, b01 - 15)
+    dimming_factor = -10
+    r01 = max(0, r01 - r0 / 6 + dimming_factor)
+    g01 = max(0, g01 - g0 / 6 + dimming_factor)
+    b01 = max(0, b01 - b0 / 6 + dimming_factor)
     return [r01, g01, b01, a01 * 255]
 
 
 def hex_to_rgb(h):
     h = h.lstrip('#')
-    return list(float(int(h[i:i + 2], 16)) for i in (0, 2, 4))
+    return list((int(h[i:i + 2], 16)) for i in (0, 2, 4))
+
+
+def saturation(c, sat):
+    tmp = c[3]
+    c = [min(254, i + 10) for i in c]
+    gray = 0.2989 * c[0] + 0.5870 * c[1] + 0.1140 * c[2]
+    c = [int(min(254.0, (max(0.0, -gray * sat + i * (1 + sat))))) for i in c]
+    c[3] = tmp
+    return c
 
 
 def DiamondAngle(y, x):
